c3c8ecc29975eadcf5751008cdc9d0ebcadd4855
kkdsdesilva <kkdsdesilva@icloud.com>
2024-04-18 13:18:03 -0700

    Remove editable mode from setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,4 @@ setup(
     version="0.1",  # Project version
     packages=find_packages(where="src"),  # Tell setuptools to find packages under src
     package_dir={"": "src"},  # Treat src as the directory where packages are found
-    editable=True,  # Install package in editable mode
 )
